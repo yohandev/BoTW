233148501066727f67b75c09b31e0f26e004469a
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-01-20 22:33:31 -0800

    init commit

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// gravity for game objects with character controllers
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public class Gravity : MonoBehaviour
+{
+    public Vector3 force = Vector3.down * 9.81f;
+
+    private CharacterController _controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _controller.SimpleMove(force * Time.deltaTime);
+    }
+}
